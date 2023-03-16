b51ef96b8187a97011a680566fa0bf7c3ec9d264
marcin10lw <marcin10lw@wp.pl>
2023-03-16 14:20:37 +0100

    used SectionTitle in NoResultsPage

--- src/common/NoResultsPage/index.js
+++ src/common/NoResultsPage/index.js
@@ -1,9 +1,9 @@
-import TilesSection from "../TilesSection";
 import { Main } from "../Main";
 import { Container } from "../Container";
 import { StyledNoResultsImage } from "./styled";
 import { useSearchParams } from "react-router-dom";
 import searchQueryParamName from "../searchQueryParamName";
+import { SectionTitle } from "../SectionTitle";
 
 const NoResultsPage = () => {
   const [searchParams] = useSearchParams({ [searchQueryParamName]: "" });
@@ -11,11 +11,12 @@ const NoResultsPage = () => {
 
   return (
     <Main>
-    <Container>
-      <TilesSection title={`Sorry, there are no results for “${query}”`} >
-        <StyledNoResultsImage />
-      </TilesSection>
-    </Container>
+      <Container>
+        <section>
+          <SectionTitle>{`Sorry, there are no results for “${query}”`}</SectionTitle>
+          <StyledNoResultsImage />
+        </section>
+      </Container>
     </Main>
   );
 };
