6013e56f31f6a7b6412b9b61062e87c5bb8dcafc
marcin10lw <marcin10lw@wp.pl>
2023-04-20 21:56:45 +0200

    simplified getData functions by creating getData

--- src/common/getData.js
+++ src/common/getData.js
@@ -3,44 +3,44 @@ import axios from "axios";
 const baseUrl = "https://api.themoviedb.org/3";
 const apiKey = `${process.env.REACT_APP_API_KEY}`;
 
+const getData = async (path, params) => {
+  const url = `${baseUrl}/${path}`;
+  const response = await axios.get(url, {
+    params: { api_key: apiKey, ...params },
+  });
+
+  return response;
+};
+
 export const getPopularData = async (dataName, currentPage) => {
-  const { data } = await axios.get(`${baseUrl}/${dataName}/popular`, {
-    params: { api_key: apiKey, page: currentPage },
+  const { data } = await getData(`${dataName}/popular`, {
+    page: currentPage,
   });
 
   return data;
 };
 
 export const getDataByQuery = async (dataName, currentPage = 1, query) => {
-  if (!query) {
-    return;
-  }
-  const { data } = await axios.get(`${baseUrl}/search/${dataName}`, {
-    params: { api_key: apiKey, page: currentPage, query },
+  if (!query) return;
+
+  const { data } = await getData(`search/${dataName}`, {
+    page: currentPage,
+    query,
   });
 
   return data;
 };
 
 export const getGenres = async () => {
-  const { data } = await axios.get(`${baseUrl}/genre/movie/list`, {
-    params: { api_key: apiKey },
-  });
+  const { data } = await getData(`genre/movie/list`);
 
   return data.genres;
 };
 
 export const getPersonData = async (id) => {
-  const { data: infoRequest } = await axios.get(`${baseUrl}/person/${id}`, {
-    params: { api_key: apiKey },
-  });
+  const { data: infoRequest } = await getData(`person/${id}`);
 
-  const { data: creditsRequest } = await axios.get(
-    `${baseUrl}/person/${id}/movie_credits`,
-    {
-      params: { api_key: apiKey },
-    }
-  );
+  const { data: creditsRequest } = await getData(`person/${id}/movie_credits`);
 
   const [personInfo, personCredits] = await axios.all([
     infoRequest,
@@ -55,16 +55,9 @@ export const getPersonData = async (id) => {
 };
 
 export const getMovieData = async (id) => {
-  const { data: infoRequest } = await axios.get(`${baseUrl}/movie/${id}`, {
-    params: { api_key: apiKey },
-  });
+  const { data: infoRequest } = await getData(`movie/${id}`);
 
-  const { data: creditsRequest } = await axios.get(
-    `${baseUrl}/movie/${id}/credits`,
-    {
-      params: { api_key: apiKey },
-    }
-  );
+  const { data: creditsRequest } = await getData(`movie/${id}/credits`);
 
   const [movieInfo, movieCredits] = await axios.all([
     infoRequest,
