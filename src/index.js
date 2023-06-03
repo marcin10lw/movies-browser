f41a8195e53f6f528dcba563d1d9f7e3402aa60f
marcin10lw <marcin10lw@wp.pl>
2023-06-03 23:13:00 +0200

    removed repoerWebVitals

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 import App from "./App";
 import store from "./store";
-import { Provider } from "react-redux";
-import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </Provider>
   </HashRouter>
 );
-
-reportWebVitals();
